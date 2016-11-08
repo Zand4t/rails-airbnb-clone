88fa772519fa5e576d91c3261bf3c25e0448fce4
Jasper Reijenga <jasper.reijenga@gmail.com>
2016-11-08 18:04:14 +0100

    adds tags controller, not sure about funcionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,38 @@
 class TagsController < ApplicationController
+
+  before_action :set_tag, only: [:create :edit, :update, :destroy]
+
+  def index
+   @tags = Tag.all
+  end
+
+  def new # GET /tag/new
+    @tag = Tag.new
+  end
+
+  def create # POST /tag
+    @tag = Tag.new(tag_params)
+    @tag.save
+  end
+
+  # def edit # GET /tag/:id/edit
+  # end
+
+  def update # PATCH /tag/:id
+    @tag.update(params[:tag])
+  end
+
+  def destroy # DELETE /tag/:id
+  end
+
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
 end
